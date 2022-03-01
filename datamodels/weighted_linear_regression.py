cae375366979f7fad50e050a1649c68318bbece2
swilfling <sandra.wilfling@student.tugraz.at>
2022-03-01 14:07:54 +0100

    Added: wrapper for scikit learn grid search

--- datamodels/weighted_linear_regression.py
+++ datamodels/weighted_linear_regression.py
@@ -3,6 +3,33 @@ import numpy as np
 
 from . import Model
 
+from statsmodels.regression.linear_model import WLS
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+
+class wls_wrapper(BaseEstimator, ClassifierMixin):
+    def __init__(self, **params):
+        self.set_params(**params)
+
+    def set_params(self, **params):
+        self.l1_wt = params.pop('l1_wt', 0)
+        self.method = params.pop('method', 'elastic_net')
+        self.alpha = params.pop('alpha', 1)
+        return self
+
+    def get_params(self, deep=True):
+        return {'l1_wt': self.l1_wt, 'method': self.method, 'alpha': self.alpha}
+
+    def fit(self, x_train, y_train, **kwargs):
+        y_train = y_train.reshape(y_train.shape[0])
+        self.estimator = WLS(y_train, x_train)
+        self.coeffs = self.estimator.fit_regularized(method=self.method, alpha=self.alpha, L1_wt=self.l1_wt, **kwargs).params
+
+    def predict(self, x_test, **kwargs):
+        if self.estimator is None:
+            raise ValueError('The model is not trained yet.')
+        return np.array(self.estimator.predict(self.coeffs, exog=x_test))
+
 
 class WeightedLS(Model):
 
@@ -10,11 +37,7 @@ class WeightedLS(Model):
         super().__init__(**kwargs)
         if parameters is None:
             parameters = {'l1_wt':0, 'method':'elastic_net', 'alpha':1}
-        self.l1_wt = parameters.pop('l1_wt', 0)
-        self.method = parameters.pop('method','elastic_net')
-        self.alpha = parameters.pop('alpha', 1)
-        self.model = None
-        self.coeffs = None
+        self.model = wls_wrapper(**parameters)
 
 
     def reshape_data(self, x):
@@ -23,16 +46,10 @@ class WeightedLS(Model):
         return x
 
     def train_model(self, x_train, y_train, **kwargs):
-        from statsmodels.regression.linear_model import WLS
-        y_train = y_train.reshape(y_train.shape[0])
-        self.model = WLS(y_train, x_train)
-        self.coeffs = self.model.fit_regularized(method=self.method, alpha=self.alpha, L1_wt = self.l1_wt).params
-        #self.coeffs = self.model.fit().params
+        self.model.fit(x_train,y_train)
 
     def predict_model(self, x):
-        if self.model is None:
-            raise ValueError('The model is not trained yet.')
-        result = np.array(self.model.predict(self.coeffs, exog=x))
+        result = self.model.predict(x)
         if result.ndim == 1:
             result = np.expand_dims(result, axis=-1)
         return result
@@ -40,9 +57,9 @@ class WeightedLS(Model):
     def save(self, path="data/models/DUMMY.txt"):
         super(WeightedLS, self).save(path)
         with open(f'{path}/model.pickle', 'wb') as file:
-            pickle.dump([self.l1_wt, self.method, self.alpha, self.model, self.coeffs], file)
+            pickle.dump([self.model], file)
 
     def load_model(self, path="data/models/DUMMY.txt"):
         super(WeightedLS, self).load_model(path)
         with open(f'{path}/model.pickle', 'rb') as file:
-            [self.l1_wt, self.method, self.alpha, self.model, self.coeffs] = pickle.load(file)
+            [self.model] = pickle.load(file)
