caf8dbc0d752365ed3c7ccc423a5bd11d8a79f39
swilfling <sandra.wilfling@student.tugraz.at>
2022-04-19 17:32:57 +0200

    added white test to validation

---
+++ datamodels/validation/white_test.py
@@ -0,0 +1,17 @@
+from statsmodels.stats import diagnostic as diag
+
+
+def white_test(x_test, residual):
+    """
+    White's Lagrange Multiplier test for heteroscedascity
+    :param x_test: Test input data
+    :param residual: Residual values (x_true - x_predicted)
+    :return: {'pvalue_lm', 'pvalue_f} - White's test statistics
+    """
+    # White - Test
+    try:
+        [_,pvalue_lm,_,pvalue_f] = diag.het_white(residual, x_test.reshape(x_test.shape[0], x_test.shape[2]))
+        return {'pvalue_lm':pvalue_lm,'pvalue_f':pvalue_f}
+    except AssertionError:
+        print('White Test: Assertion error during calculation. Results are invalid.')
+    return {}
