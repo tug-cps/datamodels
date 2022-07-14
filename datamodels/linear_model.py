f08f277535b505ea792a46d9efab33ea06209762
swilfling <sandra.wilfling@student.tugraz.at>
2022-07-14 10:19:42 +0200

    fix reshaping in linear models

--- datamodels/linear_model.py
+++ datamodels/linear_model.py
@@ -13,23 +13,29 @@ class LinearModel(Model):
         self.model = DummyRegressor()
         self.feature_names = kwargs.pop('feature_names',None)
 
-    def reshape(self, x):
+    def reshape_x(self, x):
         if x.ndim == 3:
             x = x.reshape(x.shape[0], -1)
         return x
 
+    def reshape_y(self, y):
+        if y.ndim == 1:
+            y = y.ravel()
+        if y.ndim == 3:
+            y = y.reshape(y.shape[0], -1)
+        return y
+
     def train_model(self, x_train, y_train, **kwargs):
-        if y_train.ndim == 1:
-            y_train = y_train.ravel()
         # Add feature names
-        x_train = self.reshape(x_train)
+        x_train = self.reshape_x(x_train)
+        y_train = self.reshape_y(y_train)
         if self.feature_names is not None:
             x_train = pd.DataFrame(data=x_train, columns=self.feature_names)
         self.model.fit(x_train, y_train)
 
     def predict_model(self, x):
         # Add feature names
-        x = self.reshape(x)
+        x = self.reshape_x(x)
         if self.feature_names is not None:
             x = pd.DataFrame(data=x, columns=self.feature_names)
         result = self.model.predict(x)
